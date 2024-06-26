3387df12d9678b61f6d97eb4b27ef4356d0b8e28
Bhargav Pratim <bhargavsarma173@gmail.com>
2024-06-26 20:36:12 +0530

    finalising..

---
+++ lib/questions_summary.dart
@@ -0,0 +1,36 @@
+
+import 'package:flutter/material.dart';
+
+class QuestionsSummary extends StatelessWidget {
+  const QuestionsSummary(this.summaryData, {super.key});
+  final List<Map<String, Object>> summaryData;
+
+  @override
+  Widget build(context) {
+    return SizedBox(
+      height: 230,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData.map(
+            (data) {
+              return Row(
+                children: [
+                  Text(((data['question_index'] as int) + 1).toString(), style: const TextStyle(color: Colors.white),),
+                  // expanded guarantees that the child, i.e, Column can't take more width than its parent widget, that is, Row here
+                  Expanded(
+                    child: Column(children: [
+                      Text(data['question'] as String, style: const TextStyle(color: Colors.white),),
+                      const SizedBox(height: 22,),
+                      Text(data['user_answer'] as String, style: const TextStyle(color: Colors.white),),
+                      Text(data['correct_answer'] as String, style: const TextStyle(color: Colors.white),),
+                    ],),
+                  )
+                ],
+              );
+            },
+          ).toList(),
+        ),
+      ),
+    );
+  }
+}
--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/questions_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.choosenAnswers});
+
+  List <Map<String, Object>> getSummaryData(){
+    final List <Map<String, Object>> summary = [];
+
+    for(var i=0; i < choosenAnswers.length; i++ ){
+      summary.add({
+        'question_index' : i,
+        'question' : questions[i].text,
+        'correct_answer' : questions[i].ans[0],
+        'user_answer' : choosenAnswers[i]
+      });
+    }
+    return summary;
+  }
 
   final List<String> choosenAnswers;
 
   @override
   Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numTotalQuestions = questions.length;
+    final numCorrectQuestions = summaryData.where((data){
+      return data['user_answer'] == data['correct_answer'];
+    }).length;
+  
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -14,11 +36,11 @@ class ResultsScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You answered X out of Y questions correctly!'),
+            Text('You answered $numCorrectQuestions out of $numCorrectQuestions questions correctly!', style: const TextStyle(color: Colors.white),),
             const SizedBox(
               height: 22,
             ),
-            const Text('List of answers and questions'),
+            QuestionsSummary(getSummaryData()),
             const SizedBox(
               height: 22,
             ),
